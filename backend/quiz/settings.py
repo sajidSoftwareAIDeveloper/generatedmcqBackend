8035302a3c1082be2b847a7a2983457ea140ed1a
sajidsoftwaredeveloper <141508955+sajidSoftwareAIDeveloper@users.noreply.github.com>
2025-05-10 14:58:47 +0530

    Update settings.py

--- backend/quiz/settings.py
+++ backend/quiz/settings.py
@@ -10,10 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+# Other imports
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Your other settings
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 # Quick-start development settings - unsuitable for production
